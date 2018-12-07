055b2ccfc5a4802313818ce558862a549783fbfc
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-12-07 08:41:48 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,46 +1,36 @@
-const settings = require("./config.json"); 
-const Discord = require('discord.js');
-const initcmd = settings.initcmd;
-const id = settings.id;
+const Discord = require("discord.js");
 const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Hi ${client.user.tag} , mdo `);
-  console.log('')
-  console.log(`i Have  [ " ${client.guilds.size} " ]`);
-});
-
-const developers = id
-const adminprefix = initcmd;
+var prefix = "d";
+var adminprefix = '.'
+const developers = ["520640850401820682"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
-  if (message.content.startsWith(adminprefix + 'ply')) {
+  if (message.content.startsWith(adminprefix + 'setg')) {
     client.user.setGame(argresult);
-      message.channel.send("**:white_check_mark: | The Playing Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
+      message.channel.send(`:white_check_mark: Changing The Playing Mode To **${argresult}** :white_check_mark:  `)
   } else 
-  if (message.content.startsWith(adminprefix + 'wat')) {
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send("**:white_check_mark: | The Watching Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
+      message.channel.send(`:white_check_mark: Successfully Changing The Watching Mode To **${argresult}** :white_check_mark: `)
   } else 
-  if (message.content.startsWith(adminprefix + 'lis')) {
+  if (message.content.startsWith(adminprefix + 'setl')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send("**:white_check_mark: | The Listening Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
+      message.channel.send(`:white_check_mark: Successfully Changing The Listening Mode To **${argresult}** :white_check_mark: `)
   } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/i_kahrba999");
-      message.channel.send("**:white_check_mark: | The Streaming Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/One");
+      message.channel.send(`:white_check_mark: Successfully Changing The Streaming Mode To **${argresult}** :white_check_mark: `)
   }
-  if (message.content.startsWith(adminprefix + 'setname')) {
+  if (message.content.startsWith(adminprefix + 'setn')) {
   client.user.setUsername(argresult).then
       message.channel.send(`Changing The Name To ..**${argresult}** `)
 } else
-if (message.content.startsWith(adminprefix + 'setavatar')) {
+if (message.content.startsWith(adminprefix + 'seta')) {
   client.user.setAvatar(argresult);
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
@@ -48,5 +38,4 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
 
 
 
-
-client.login(settings.token);
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
