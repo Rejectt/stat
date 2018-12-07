96759dd470bef30443d7c8aa6823507b9b6e6557
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-12-07 09:08:41 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,49 +1,58 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const adminprefix = "m-";
+const developers = ["504048353483423746","509689390528987158"]
+const prefix = "m-";
 client.on('ready', () => {
-  console.log(`Hi ${client.user.tag} , This Code by : MeDo `);
+  console.log('╔[════════════════════════════════════]╗');
   console.log('')
-  console.log(`i Have  [ " ${client.guilds.size} " ]`);
+  console.log('            ╔[════════════]╗')
+  console.log('              Bot Is Online')
+  console.log('            ╚[════════════]╝')
+  console.log('')
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('')
+  console.log('By Unknown')
+  console.log('')  
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('')
+  console.log('╚[════════════════════════════════════]╝')
 });
 
-const developers = id
-const adminprefix = 'r'
+
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
-  if (message.content.startsWith(adminprefix + 'ply')) {
+  if (message.content.startsWith(adminprefix + 'sp')) {
     client.user.setGame(argresult);
-      message.channel.send("**:white_check_mark: | The Playing Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
-  } else 
-  if (message.content.startsWith(adminprefix + 'wat')) {
+      message.channel.send(`**Done You Have Been Changed The Playing To ${argresult}**✅ `)
+  } else  
+  if (message.content.startsWith(adminprefix + 'sw')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send("**:white_check_mark: | The Watching Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
+      message.channel.send(`**Done You Have Been Changed The Watching To ${argresult}**✅`)
   } else 
-  if (message.content.startsWith(adminprefix + 'lis')) {
+  if (message.content.startsWith(adminprefix + 'sl')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send("**:white_check_mark: | The Listening Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
+      message.channel.send(`**Done You Have Been Changed The Listening To ${argresult}**✅`)
   } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/i_kahrba999");
-      message.channel.send("**:white_check_mark: | The Streaming Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
+  if (message.content.startsWith(adminprefix + 'ss')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**Done You Have Been Changed The Streaming To ${argresult}**✅`)
   }
-  if (message.content.startsWith(adminprefix + 'setname')) {
+  if (message.content.startsWith(adminprefix + 'sn')) {
   client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
+      message.channel.send(`**Changing The Name To ..${argresult}** ✅`)
 } else
-if (message.content.startsWith(adminprefix + 'setavatar')) {
+if (message.content.startsWith(adminprefix + 'sa')) {
   client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+    message.channel.send(`**Changing The Avatar To :${argresult}** ✅`);
 }
+
+
 });
-
-
 
 
 client.login(process.env.BOT_TOKEN);
